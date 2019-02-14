0863e432f95e1270dc2c95bb9de81b2ade01238d
ruesin <ruesin@gmail.com>
2019-02-14 14:00:43 +0800

    check process_name before start server

--- src/Server.php
+++ src/Server.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Swover;
 
 use Swover\Server\Process;
@@ -25,7 +26,7 @@ class Server
             die('server_type defined error!' . PHP_EOL);
         }
 
-        if (!isset($this->config['process_name']) ) {
+        if (!isset($this->config['process_name'])) {
             die('process_name defined error!' . PHP_EOL);
         }
     }
@@ -35,6 +36,15 @@ class Server
      */
     public function start()
     {
+        if (!empty($this->getAllPid())) {
+            echo 'Process names[' . $this->config['process_name'] . '] already exists, you have to wait 5 seconds for confirmation or it will start normally.' . PHP_EOL;
+            for ($i = 1; $i <= 5; $i++) {
+                sleep(1);
+                echo $i . ' ';
+            }
+            echo PHP_EOL;
+        }
+        echo "Starting {$this->config['process_name']} ..." . PHP_EOL;
         if ($this->config['server_type'] == 'process') {
             new Process($this->config);
         } else {
@@ -50,15 +60,15 @@ class Server
         $pid = $this->getPid('master');
 
         if (empty($pid)) {
-            die("{$this->config['process_name']} has not process".PHP_EOL);
+            die("{$this->config['process_name']} has not process" . PHP_EOL);
         }
 
-        exec("kill -15 ".implode(' ', $pid), $output, $return);
+        exec("kill -15 " . implode(' ', $pid), $output, $return);
 
         if ($return === false) {
-            die("{$this->config['process_name']} stop fail".PHP_EOL);
+            die("{$this->config['process_name']} stop fail" . PHP_EOL);
         }
-        echo "{$this->config['process_name']} stop success".PHP_EOL;
+        echo "{$this->config['process_name']} stop success" . PHP_EOL;
     }
 
     /**
@@ -73,11 +83,11 @@ class Server
                 $stopped = true;
                 break;
             }
-            sleep(mt_rand(1,3));
+            sleep(mt_rand(1, 3));
         }
 
         if (!$stopped) {
-            die("{$this->config['process_name']} has not stopped".PHP_EOL);
+            die("{$this->config['process_name']} has not stopped" . PHP_EOL);
         }
         $this->start();
     }
@@ -94,15 +104,15 @@ class Server
         }
 
         if (empty($pid)) {
-            die("{$this->config['process_name']} has not process".PHP_EOL);
+            die("{$this->config['process_name']} has not process" . PHP_EOL);
         }
 
         exec("kill -USR1 " . implode(' ', $pid), $output, $return);
 
         if ($return === false) {
-            die("{$this->config['process_name']} reload fail".PHP_EOL);
+            die("{$this->config['process_name']} reload fail" . PHP_EOL);
         }
-        echo "{$this->config['process_name']} reload success".PHP_EOL;
+        echo "{$this->config['process_name']} reload success" . PHP_EOL;
     }
 
     /**
@@ -110,7 +120,7 @@ class Server
      */
     private function getAllPid()
     {
-        $types = ['master','manager','worker'];
+        $types = ['master', 'manager', 'worker'];
         $pids = [];
         foreach ($types as $type) {
             $pids = array_merge($pids, $this->getPid($type));
