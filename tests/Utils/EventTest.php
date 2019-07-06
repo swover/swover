36429be51f568bfeb5ec5548ca707e0f001dea0a
ruesin <ruesin@gmail.com>
2019-07-06 17:35:14 +0800

    add event tests

--- tests/Utils/EventTest.php
+++ tests/Utils/EventTest.php
@@ -24,6 +24,13 @@ class EventTest extends TestCase
         $this->assertEquals(4, $bounds);
     }
 
+    public function testRegisterNoArray()
+    {
+        $events = '\Swover\Tests\Utils\TestMasterStart';
+        $bounds = Event::getInstance()->register($events);
+        $this->assertEquals(1, $bounds);
+    }
+
     public function testBind()
     {
         $events = [
@@ -35,8 +42,20 @@ class EventTest extends TestCase
         $instance->register($events);
 
         $instance->bind(new TestWorkerStartB());
+
         $instance->trigger(WorkerStart::EVENT_TYPE, 100);
         $this->expectOutputString('a100b100');
+    }
+
+    public function testBindInstance()
+    {
+        $instance = Event::getInstance();
+        $instance->clear();
+
+        $instance->bindInstance('special_event', 'special_alias', new TestWorkerStartB());
+
+        $instance->trigger('special_event', 100);
+        $this->expectOutputString('b100');
     }
 
     public function testBefore()
@@ -61,6 +80,7 @@ class EventTest extends TestCase
         ];
 
         $instance = Event::getInstance();
+        $instance->clear();
         $instance->register($events);
         $instance->trigger(TaskStart::EVENT_TYPE, 300, 'data');
         $this->expectOutputString('300:data');
@@ -73,11 +93,30 @@ class EventTest extends TestCase
             new TestWorkerStartB()
         ];
         $instance = Event::getInstance();
+        $instance->clear();
         $instance->register($events);
 
         $instance->remove(new TestWorkerStartA());
 
         $instance->trigger(WorkerStart::EVENT_TYPE, 400);
+        $this->expectOutputString('b400');
+    }
+
+    public function testRemoveAlias()
+    {
+        $events = [
+            new TestWorkerStartA(),
+            new TestWorkerStartB()
+        ];
+        $instance = Event::getInstance();
+        $instance->clear();
+
+        $instance->register($events);
+
+        $instance->removeAlias(WorkerStart::EVENT_TYPE, TestWorkerStartA::class);
+
+        $instance->trigger(WorkerStart::EVENT_TYPE, 400);
+
         $this->expectOutputString('b400');
     }
 
