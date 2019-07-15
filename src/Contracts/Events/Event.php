88dbc2f35e74fe5ecd4f990c5807ff1b8b03ab47
ruesin <ruesin@gmail.com>
2019-07-15 16:47:16 +0800

    add comments

--- src/Contracts/Events/Event.php
+++ src/Contracts/Events/Event.php
@@ -4,18 +4,53 @@ namespace Swover\Contracts\Events;
 
 interface Event
 {
+    /**
+     * @see \Swover\Contracts\Events\Request
+     */
+    const REQUEST = 'request';
+
+    /**
+     * @see \Swover\Contracts\Events\Response
+     */
+    const RESPONSE = 'response';
+
+    /**
+     * @see \Swoole\Server::$onStart
+     */
     const MASTER_START = 'master_start';
+
+    /**
+     * @see \Swoole\Server::$onManagerStart
+     */
     const MANAGER_START = 'manager_start';
 
+    /**
+     * @see \Swoole\Server::$onWorkerStart
+     */
     const WORKER_START = 'worker_start';
+
+    /**
+     * @see \Swoole\Server::$onWorkerStop
+     */
     const WORKER_STOP = 'worker_stop';
 
+    /**
+     * @see \Swoole\Server::$onTask
+     */
     const TASK_START = 'task_start';
+
+    /**
+     * @see \Swoole\Server::$onFinish
+     */
     const TASK_FINISH = 'task_finish';
 
+    /**
+     * @see \Swoole\Server::$onConnect
+     */
     const CONNECT = 'connect';
-    const CLOSE = 'close';
 
-    const REQUEST = 'request';
-    const RESPONSE = 'response';
+    /**
+     * @see \Swoole\Server::$onClose
+     */
+    const CLOSE = 'close';
 }
